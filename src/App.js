2ee366551044ad73e69cbaf5197a5927d4f8de37
Nguyễn Thành Nam <bffkudo1412@gmail.com>
2024-01-16 13:13:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
 import { useState } from "react";
-
-export default function MyApp() {
-  const [count, setCount] = useState(0);
+// ô vuông
+function Square({ value, onSquareClick }) {
   return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton count={count} setCount={setCount} />
-      <MyButton count={count} setCount={setCount} />
-    </div>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+// bảng
+function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    //kiểm tra xem ô thứ i đã được tích hay chưa hoặc nếu win rồi thì sẽ dừng
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquare = squares.slice();
+    if (xIsNext) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "O";
+    }
+
+    setSquares(nextSquare);
+    setXIsNext(!xIsNext);
+  }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "winer" + winner;
+  } else {
+    status = "next player" + (xIsNext ? "X" : "O");
+  }
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
   );
 }
 
-function MyButton(props) {
-  function handleClick() {
-    props.setCount(props.count + 1);
+function calculateWinner(square) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (square[a] && square[a] == square[b] && square[a] == square[c]) {
+      return square[a];
+    }
   }
-
-  return <button onClick={handleClick}>Clicked {props.count} times</button>;
+  return null;
+}
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+import "./App.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
